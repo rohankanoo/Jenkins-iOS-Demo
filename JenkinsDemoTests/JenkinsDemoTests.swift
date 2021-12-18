bf7a52b08e7638182afd063f04f10d11a72e44f1
Rohan Kanoo <rohan.kanoo@gmail.com>
2021-12-18 12:28:25 +0530

    added unit test cases

--- JenkinsDemoTests/JenkinsDemoTests.swift
+++ JenkinsDemoTests/JenkinsDemoTests.swift
@@ -21,6 +21,7 @@ class JenkinsDemoTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(2, 3, "Test failed")
     }
 
     func testPerformanceExample() throws {
